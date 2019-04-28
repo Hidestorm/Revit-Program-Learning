8c9befce98551a1181b0a5dc68e090b4ddfda7c6
xuj-n <xuj-n@glodon.com>
2019-04-28 15:12:57 +0800

    Lesson 17.1 – Running the game on another computer

--- xuj-n/C#/SuperAdventure/SuperAdventure/SuperAdventure.Designer.cs
+++ xuj-n/C#/SuperAdventure/SuperAdventure/SuperAdventure.Designer.cs
@@ -158,6 +158,7 @@
             this.btnUseWeapon.TabIndex = 11;
             this.btnUseWeapon.Text = "Use";
             this.btnUseWeapon.UseVisualStyleBackColor = true;
+            this.btnUseWeapon.Click += new System.EventHandler(this.btnUseWeapon_Click);
             // 
             // btnUsePotion
             // 
@@ -167,6 +168,7 @@
             this.btnUsePotion.TabIndex = 12;
             this.btnUsePotion.Text = "Use";
             this.btnUsePotion.UseVisualStyleBackColor = true;
+            this.btnUsePotion.Click += new System.EventHandler(this.btnUsePotion_Click);
             // 
             // btnNorth
             // 
